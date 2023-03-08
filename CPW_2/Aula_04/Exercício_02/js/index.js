bd580ca15bf0844b565518439a710c755ba92128
MMCAC <43833424+MMCAC@users.noreply.github.com>
2023-03-08 16:14:08 -0400

    Atualização do projeto

--- CPW_2/Aula_04/Exercício_02/js/index.js
+++ CPW_2/Aula_04/Exercício_02/js/index.js
@@ -2,13 +2,21 @@
 const $ = document.querySelector.bind(document)
 const $all = document.querySelectorAll.bind(document)
 
-const onChangeRangesRGBA = () => {
+const onChangeRangesRGBA_HSL = () => {
     const red = $('#red').value; 
     const green = $('#green').value;
     const blue = $('#blue').value;
     const alpha = $('#alpha').value;
 
     $('#color-display-rgba').style.backgroundColor = `rgba(${red},${green},${blue},${alpha})`;
+
+    const hue = $('#hue').value;
+    const saturation = $('#saturation').value;
+    const lightnees = $('#lightness').value;
+    console.log(`${hue},${saturation},${lightnees}`)
+
+    $('#color-display-hsl').style.backgroundColor = `hsl(${hue}deg,${saturation}%,${lightnees}%)`;
+    
 }
 
 // $('#red').oninput = onChangeRangesRGBA
@@ -23,7 +31,9 @@ const onChangeRangesRGBA = () => {
  * for(ler i = 0; i < tamanaho; i++)
  */
 
-$all('#color-ranges-rgba input[type=range]').forEach((range) => range.oninput = onChangeRangesRGBA)
+$all('#color-ranges-rgba input[type=range]').forEach((range) => range.oninput = onChangeRangesRGBA_HSL)
+
+$all('#color-ranges-hsl input[type=range]').forEach((range) => range.oninput = onChangeRangesRGBA_HSL)
 
 /**
  * rgb(128,128,128)
